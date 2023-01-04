44e886011f54977f3af9f4907229e8931931471e
Lorenzo "labihi" Garibaldi <lorenzo.garibaldi@itj.it>
2023-01-04 21:54:53 +0100

    fix: fixed the colors in tailwind to extend the standard palette

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,17 +2,18 @@
 module.exports = {
   content: ["./src/**/*.{html,js,ts}"],
   theme: {
-    colors: {
-      almost_white: '#e6e5ea',
-      light_grey: '#817d92',
-      dark_grey: '#24232c',
-      very_dark_grey: '#18171f',
-      neon_green_strong: '#a4ffaf',
-      yellow_medium: '#f8cd65',
-      orange_weak: '#fb7c58',
-      red_too_weak: '#f64a4a',
+    extend: {
+      colors: {
+        almost_white: '#e6e5ea',
+        light_grey: '#817d92',
+        dark_grey: '#24232c',
+        very_dark_grey: '#18171f',
+        neon_green_strong: '#a4ffaf',
+        yellow_medium: '#f8cd65',
+        orange_weak: '#fb7c58',
+        red_too_weak: '#f64a4a',
+      },
     },
-    extend: {},
   },
   plugins: [],
 }
