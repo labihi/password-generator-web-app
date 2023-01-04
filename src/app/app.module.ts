23fa8caa9c9f6de39a9ab6581617ba2f3704bae5
Lorenzo "labihi" Garibaldi <lorenzo.garibaldi@itj.it>
2023-01-04 22:00:53 +0100

    feat: added main container component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GlobalContainerComponent } from './components/global-container/global-container.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GlobalContainerComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/global-container/global-container.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-global-container',
+  templateUrl: './global-container.component.html',
+  styleUrls: ['./global-container.component.scss']
+})
+export class GlobalContainerComponent {
+
+}
