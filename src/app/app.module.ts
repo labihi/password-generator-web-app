94df739f57a5a55096054e71953e0d369e95d620
Lorenzo "labihi" Garibaldi <lorenzo.garibaldi@itj.it>
2023-01-06 11:20:59 +0100

    feat: started working on the functionality

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GlobalContainerComponent } from './components/global-container/global-container.component';
+import {FormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [
     AppComponent,
     GlobalContainerComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        FormsModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
--- src/app/components/global-container/global-container.component.ts
+++ src/app/components/global-container/global-container.component.ts
@@ -7,4 +7,114 @@ import { Component } from '@angular/core';
 })
 export class GlobalContainerComponent {
 
+  uppercase: string = '';
+  lowercase: string = '';
+  numbers: string = '';
+  symbols: string = '';
+
+  Allowed = {
+    Uppers: "QWERTYUIOPASDFGHJKLZXCVBNM",
+    Lowers: "qwertyuiopasdfghjklzxcvbnm",
+    Numbers: "1234567890",
+    Symbols: "!@#$%^&*",
+  };
+
+  textBarValue: string = '';
+  passwordLengthValueText: string = '';
+
+  totalOptions = 0;
+
+  generateAll() {
+
+    const options = {
+      uppercase: this.uppercase,
+      lowercase: this.lowercase,
+      numbers: this.numbers,
+      symbols: this.symbols,
+    };
+
+    this.generatePassword({ length, ...options });
+    this.generateSecurityBar(options, length);
+  }
+
+  private generatePassword(
+    options: {
+      uppercase: string;
+      lowercase: string;
+      length: number;
+      numbers: string;
+      symbols: string
+    }) {
+
+    let pwd = "";
+
+    for (let i = 0; i < options.length; i++) {
+      if (
+        options.lowercase &&
+        options.uppercase &&
+        options.numbers &&
+        options.symbols
+      )
+        pwd += this.getRandomCharFromString(Object.values(this.Allowed).join(""));
+      else {
+        // Destructure the Allowed object in order to get the allowed characters
+        const { Uppers, Lowers, Numbers, Symbols } = this.Allowed;
+        let allowedChars = "";
+        // Add the allowed characters to the allowedChars string
+        if (options.uppercase) allowedChars += Uppers;
+        if (options.lowercase) allowedChars += Lowers;
+        if (options.numbers) allowedChars += Numbers;
+        if (options.symbols) allowedChars += Symbols;
+        // Add a random character from the allowedChars string to the password
+        pwd += this.getRandomCharFromString(allowedChars);
+      }
+    }
+  }
+
+  private getRandomCharFromString(str: string) {
+    return str.charAt(Math.floor(Math.random() * str.length));;
+  }
+
+  private generateSecurityBar(
+    options: {
+      uppercase: string;
+      lowercase: string;
+      numbers: string;
+      symbols: string
+    }, length: number) {
+
+    this.totalOptions = 0;
+
+    //  Get the total number of enabled options
+    Object.keys(options).forEach((key) => {
+      if (options[key as keyof typeof options]) this.totalOptions++;
+    });
+
+    //  Add the style to the bar
+    switch (this.totalOptions) {
+      case 1:
+        this.textBarValue = 'TOO WEAK!';
+        break;
+      case 2:
+        this.textBarValue = 'WEAK';
+        break;
+      case 3:
+        this.textBarValue = 'MEDIUM';
+        break;
+      case 4:
+        this.textBarValue = 'STRONG';
+        break;
+      default:
+        this.textBarValue = 'TOO WEAK!';
+        break;
+    }
+  }
+
+  changePasswordLength($event: Event) {
+    this.passwordLengthValueText = ($event.target as HTMLInputElement).value;
+  }
+
+  shoudlDisableGenerateButton() {
+    return this.totalOptions === 0;
+  }
 }
